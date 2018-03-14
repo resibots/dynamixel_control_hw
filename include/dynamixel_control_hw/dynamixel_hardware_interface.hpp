1fcaf7052277b2270080348a28223d307de7300c
Dorian Goepp <dorian.goepp@inria.fr>
2018-03-14 15:23:19 +0100

    Stricter checks on command interface mode

--- include/dynamixel_control_hw/dynamixel_hardware_interface.hpp
+++ include/dynamixel_control_hw/dynamixel_hardware_interface.hpp
@@ -188,20 +188,19 @@ namespace dynamixel {
                         = _c_mode_map.find(id);
                     if (c_mode_map_i != _c_mode_map.end()) {
                         if (c_mode_map_i->second != hardware_mode) {
-                            ROS_ERROR_STREAM("the command interface declared "
+                            ROS_ERROR_STREAM("The command interface declared "
                                 << mode2str(c_mode_map_i->second)
                                 << " for joint " << dynamixel_iterator->second
                                 << " but is set to " << mode2str(hardware_mode)
-                                << " in hardware. Using the one from hardware.");
-                            _c_mode_map[id] = hardware_mode;
+                                << " in hardware. Disabling this joint.");
+                            _c_mode_map[id] = OperatingMode::unknown;
                         }
                     }
                     else {
-                        ROS_WARN_STREAM("the command interface was not defined "
+                        ROS_ERROR_STREAM("The command interface was not defined "
                             << "for joint " << dynamixel_iterator->second
-                            << " fallback to the hardware's mode: "
-                            << mode2str(hardware_mode));
-                        _c_mode_map[id] = hardware_mode;
+                            << ". Disabling this joint.");
+                        _c_mode_map[id] = OperatingMode::unknown;
                     }
 
                     // tell ros_control the in-memory address to change to set new
@@ -216,9 +215,11 @@ namespace dynamixel {
                     else if (OperatingMode::wheel == hardware_mode) {
                         _jnt_vel_interface.registerHandle(cmd_handle);
                     }
-                    else
+                    else if (OperatingMode::unknown != hardware_mode)
                         ROS_ERROR_STREAM("Servo " << id << " was not initialised "
-                                                  << "(operating mode not supported)");
+                                                  << "(operating mode "
+                                                  << mode2str(hardware_mode)
+                                                  << " is not supported)");
 
                     // enable torque output on the servo and set its configuration
                     // including max speed
